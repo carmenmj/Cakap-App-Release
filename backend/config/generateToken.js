20323f9427879f48a86b49d43ebd38b54f8d2564
wendyelmira <wendy.e.ramly@gmail.com>
2025-09-17 19:07:45 +0800

    add comment2

--- backend/config/generateToken.js
+++ backend/config/generateToken.js
@@ -1,9 +1,12 @@
 const jwt = require("jsonwebtoken");
 
+// Function to generate a token for a user based on their ID
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
 };
 
+
+// export the function to use in other files
 module.exports = generateToken;
