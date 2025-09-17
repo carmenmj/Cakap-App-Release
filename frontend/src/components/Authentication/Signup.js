cac04a668fb58b264b33bec80cb60f4d1ccfe228
chelseaslocalfiles <103267894+chelseaslocalfiles@users.noreply.github.com>
2025-09-17 22:33:07 +0800

    ----------

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -3,7 +3,7 @@ import { FormControl, FormLabel } from "@chakra-ui/form-control";
 import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
 import { VStack } from "@chakra-ui/layout";
 import { useToast } from "@chakra-ui/toast";
-import axios from "axios";
+ // from "axios";
 import { useState } from "react";
 //import { useHistory } from "react-router";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -5,7 +5,7 @@ import "./styles.css";
 import { IconButton, Spinner, useToast } from "@chakra-ui/react";
 import { getSender, getSenderFull } from "../config/ChatLogics";
 import { useEffect, useState } from "react";
-import axios from "axios";
+ // from "axios";
 import { ArrowBackIcon } from "@chakra-ui/icons";
 import ProfileModal from "./miscellaneous/ProfileModal";
 import ScrollableChat from "./ScrollableChat";
